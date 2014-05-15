f4369a1296ceb50d6da5792e378ba6b91e48cc68
Rachel Logie <rachel.logie1@gmail.com>
2014-05-15 14:59:09 -0600

    [#71354100] User can edit their own event
    Signed-off-by: Steve Magelowitz <steven.magelowitz@gmail.com>

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,13 +15,30 @@ class EventsController < ApplicationController
   end
 
   def create
-    @user = User.find(session[:current_user_id])
+    @user = User.find(session[:current_user_id]) if session[:current_user_id]
     Event.create(event_params.merge(:user => @user))
     redirect_to @user
   end
 
   def show
+    @user = User.find(session[:current_user_id]) if session[:current_user_id]
     @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+    user = User.find(session[:current_user_id]) if session[:current_user_id]
+    if @event.user.id != user.id
+      flash[:notice] = "You can't be here"
+      redirect_to '/events'
+    end
+  end
+
+  def update
+    @user = User.find(session[:current_user_id]) if session[:current_user_id]
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to @event
   end
 
   private
