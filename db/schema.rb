eb80f009d337c72cbb5320e13b591158de24322c
vandosant <sjskender@gmail.com>
2014-06-02 12:09:22 -0600

    Updated schema with users token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522210101) do
+ActiveRecord::Schema.define(version: 20140527225502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20140522210101) do
   create_table "users", force: true do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "token"
   end
 
 end
