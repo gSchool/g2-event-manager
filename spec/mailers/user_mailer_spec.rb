09f2fefc5374ec3ad5a828c7889586ce65fb265d
Paul Wenig <pwenig@gmail.com>
2014-06-05 11:40:53 -0600

    [#71354084]Fixed test to reflect change in email sender
    Signed-off-by: Seth Musulin <sethmusulin@gmail.com>

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -5,7 +5,7 @@ describe UserMailer do
     token = SecureRandom.uuid
     user = User.new(email: 'me@example.com', password: 'Password1', token: token)
     email = UserMailer.forgot_password(user).deliver
-    expect(email.from).to eq ['info@example.com']
+    expect(email.from).to eq ['info@eventmanager.com']
     expect(email.to).to eq ['me@example.com']
     expect(email.subject).to eq 'Password reset'
     expect(email.body).to have_content 'Click the link to reset password'
