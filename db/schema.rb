9fee8c032f53c63f2e8090aa811d15d684c467a1
Scott Skender <sjskender@gmail.com>
2014-06-09 16:29:07 -0600

    [#72898846] Users have a unique calendar token
    * Reorganized dates on user events show page.
    
    Signed-off-by: Kaylee Edmonson <kaylee.edmonson@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605201430) do
+ActiveRecord::Schema.define(version: 20140609220032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20140605201430) do
     t.string   "token"
     t.datetime "password_reset_sent_at"
     t.boolean  "email_confirmed",        default: false
+    t.string   "calendar_token"
   end
 
 end
