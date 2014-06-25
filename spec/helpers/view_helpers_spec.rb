e75fc21d93984fc09b811dbb08283a564566cbdf
Emily Platzer <emilyplatzer@gmail.com>
2014-06-25 15:13:22 -0600

    [#73511344] Events at midnight no longer render as 0am but as 12 am
    Signed-off-by: Nathanael Burt <nathanael.burt@gmail.com>

--- spec/helpers/view_helpers_spec.rb
+++ spec/helpers/view_helpers_spec.rb
@@ -4,6 +4,7 @@ describe ViewHelpers do
 
   it 'can format time from military time to a 12 hour clock' do
     expect(time_to_12_hour("01:00")).to eq "1am"
+    expect(time_to_12_hour("00:00")).to eq "12am"
     expect(time_to_12_hour("14:00")).to eq "2pm"
   end
 
